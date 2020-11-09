924e285958b6204b246eac5be310c27eebfa3612
Will Hoover <70659810+LordGuacaba@users.noreply.github.com>
2020-11-09 11:02:11 -0500

    Added comments for ease of use

--- cryptics.py
+++ cryptics.py
@@ -1,12 +1,15 @@
+# Gets all of the information (message, encryption key, function, and file for output) necessary to run the program
 message = input("Enter original message: ")
 keyword = input("Enter keyword: ")
 function = input("encrypt or decrypt? ")
 file_name = input("File for output: ")
 
+# Sets up a list of all characters in the message
 in_list = []
 for char in message:
   in_list.append(char)
 
+# Creates the list used to shift characters for the encryption based on the key, and adds those characters once to the reference list
 keylist = []
 shiftlist = []
 for lett in keyword:
@@ -14,16 +17,20 @@ for lett in keyword:
   if lett not in keylist:
     keylist.append(lett)
 
+# Adds all ASCII characters to conveyor list
 conveyor = []
 for num in range(32, 127):
   conveyor.append(chr(num))
 
+# Adds all ASCII characters not already in the reference list to it
 for letter in conveyor:
   if letter not in keylist:
     keylist.append(letter)
- 
+
+# This list will be for characters to output
 out_list = []  
 
+# Takes the list of characters in the message, shifts each character over in the reference list based on the letters in the key, and adds the new characters to the output list
 def encrypt_message(thing):
   shift_count = 1
   for char in thing:
@@ -37,6 +44,7 @@ def encrypt_message(thing):
     new_letter = keylist[(tp - 32)]
     out_list.append(new_letter)
 
+# Performs the same function as the last function, but shifts the letters backwards, so this is used for decryption
 def decrypt_message(stuff):
   shift_count = 1
   for char in in_list:
@@ -50,17 +58,19 @@ def decrypt_message(stuff):
     new_letter = chr(tp)
     out_list.append(new_letter)       
 
+# Based on the user-inputted function, either encrypts or decrypts the message, creating the appropriate output list
 if function == "encrypt":
   encrypt_message(in_list)
 elif function == "decrypt":
   decrypt_message(in_list)
   
+# Converts the output list to a string, adding each character, and then printing it to the console
 message_out = ""
 for letter in out_list:
   message_out += letter
-  
 print(message_out)
 
+# If a file for output was given, writes the message in that file
 if file_name != "none":
   rum = open(file_name, "w")
   rum.write(message_out)
